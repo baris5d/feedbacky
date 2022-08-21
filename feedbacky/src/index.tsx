07bd3c404bcbcef9724551c288a665fc19558144
baris5d <baris@barisdede.com>
2022-08-21 18:35:31 +0300

    chore(feedbacky): init

---
+++ feedbacky/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Feedbacky from './Feedbacky';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Feedbacky />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
