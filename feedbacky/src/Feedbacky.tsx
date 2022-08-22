7df2514b43bf0283dcc355104cb2c7d0f160a5f9
baris5d <baris@barisdede.com>
2022-08-22 16:15:36 +0300

    feat(feedbacky): init feedback modal basics

--- feedbacky/src/Feedbacky.tsx
+++ feedbacky/src/Feedbacky.tsx
@@ -1,9 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react'
+import FeedBackIcon from './feedback-icon.svg'
 
-export default function Feedbacky() {
+const Feedbacky = () => {
+    const [isToggle, setIsToggle] = useState(false)
+
     return (
         <>
-           
+            <button
+                className="button"
+                onClick={() => {
+                    setIsToggle(!isToggle)
+                }}
+            >
+                <img src={FeedBackIcon} alt="Feedback" />
+            </button>
+            {isToggle && (
+                <FeedbackyModal
+                    toggle={() => {
+                        setIsToggle(!isToggle)
+                    }}
+                />
+            )}
         </>
-    );
+    )
 }
+
+type ModalTypes = {
+    toggle: () => void
+    title?: string
+    description?: string
+    maxLength?: number
+}
+
+const FeedbackyModal = ({
+    toggle,
+    title,
+    description,
+    maxLength,
+}: ModalTypes) => {
+    const [message, setMessage] = useState('')
+
+    const submit = () => {
+        toggle()
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value)
+    }
+
+    const messageLength = () => {
+        return message.length
+    }
+
+    const getClassName = (maxLength: number) => {
+        if (messageLength() === maxLength) {
+            return '--danger'
+        } else if (messageLength() > (maxLength / 5) * 4) {
+            return '--warning'
+        }
+    }
+
+    return (
+        <div className="modal">
+            <div className="modal__content">
+                <span className="modal__close-button" onClick={toggle}>
+                    &times;
+                </span>
+                <div className="modal__header">
+                    <h2>{title ? title : 'Send your feedback'}</h2>
+                </div>
+                <div className="modal__body">
+                    <div className="modal__form-group --center">
+                        <span>
+                            {description
+                                ? description
+                                : "We'd love to hear from you!"}
+                        </span>
+                    </div>
+                    <div className="modal__form-group">
+                        <textarea
+                            name="message"
+                            onChange={handleChange}
+                            maxLength={maxLength || 2000}
+                        ></textarea>
+                        <label className={getClassName(maxLength || 2000)}>
+                            {messageLength()} / {maxLength || 2000}{' '}
+                        </label>
+                    </div>
+                    <div className="modal__form-group --right">
+                        <button
+                            className="modal__submit"
+                            onClick={() => {
+                                submit()
+                            }}
+                            disabled={message.length === 0}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Feedbacky
--- feedbacky/src/index.tsx
+++ feedbacky/src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Feedbacky from './Feedbacky';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Feedbacky from './Feedbacky'
+import './feedbacky.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <Feedbacky />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
