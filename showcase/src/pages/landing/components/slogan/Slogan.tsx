be804e7930007db5b27bb3d89560473d13b23680
baris5d <baris@barisdede.com>
2022-08-22 16:42:34 +0300

    feat(showcase): create slogan component

---
+++ showcase/src/pages/landing/components/slogan/Slogan.tsx
@@ -0,0 +1,15 @@
+import './Slogan.css';
+
+const Slogan = () => {
+    return (
+        <div className="slogan">
+            <div className="slogan__content container">
+                <h1 className="slogan__title">
+                    Getting feedback is great, but it's even better when it's combined with Feedbacky
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Slogan;
