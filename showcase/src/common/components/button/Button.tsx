a0efbae6781b048fd8c2d43abe7fa2e44b3cb061
baris5d <baris@barisdede.com>
2022-08-24 20:30:56 +0300

    refactor(showcase): button styling and props

--- showcase/src/common/components/button/Button.tsx
+++ showcase/src/common/components/button/Button.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import './Button.css';
+import { Link } from 'react-router-dom'
+import './Button.css'
 
 interface ButtonProps {
     text: string
@@ -7,16 +7,30 @@ interface ButtonProps {
     disabled?: boolean
     onClick?: () => void
     url?: string
+    type?: 'small' | 'medium'
 }
 
 const Button = (props: ButtonProps) => {
-    return (
-        props.url ? 
-            <Link to={props.url} className={`button ${props.className}`}>{props.text}</Link> :
-            <button className={`button ${props.className}`} onClick={props.onClick} disabled={props.disabled}>
-                {props.text}
-            </button>
-    );
+    return props.url ? (
+        <Link
+            to={props.url}
+            className={`button ${props.className ? props.className : ''}${
+                props.type ? ' button--' + props.type : ''
+            }`}
+        >
+            {props.text}
+        </Link>
+    ) : (
+        <button
+            className={`button ${props.className ? props.className : ''}${
+                props.type ? ' button--' + props.type : ''
+            }`}
+            onClick={props.onClick}
+            disabled={props.disabled}
+        >
+            {props.text}
+        </button>
+    )
 }
 
-export default Button;
+export default Button
