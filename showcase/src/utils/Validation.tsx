f1b4384f71c96b8513b4499d00008f2e679df202
baris5d <baris@barisdede.com>
2022-08-27 17:44:43 +0300

    feat(showcase): validation util

---
+++ showcase/src/utils/Validation.tsx
@@ -0,0 +1,11 @@
+export default class Validation {
+    static isEmail(email: string) {
+        var re =
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(String(email).toLowerCase())
+    }
+
+    static passwordLength(password: string) {
+        return password.length >= 8 && password.length <= 20
+    }
+}
