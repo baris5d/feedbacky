ec10f5c75946cd455fcdd4bdd7b5942b3f6fa2cc
baris5d <baris@barisdede.com>
2022-08-28 20:12:34 +0300

    feat(backend): create application model and resource

---
+++ backend/models/application.py
@@ -0,0 +1,64 @@
+from typing import NoReturn
+from db import db
+import uuid
+
+
+
+class ApplicationModel(db.Model):
+    __tablename__ = 'applications'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(80), nullable=False)
+    hash = db.Column(db.String(80), nullable=False)
+
+
+    user = db.relationship('UserModel')
+
+    def __init__(self, user_id, name) -> None:
+        self.user_id: str = user_id
+        self.name: str = name
+        self.hash: str = self.generate_hash()
+
+    def __repr__(self):
+        return '<Application %r>' % self.id
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, data: dict) -> NoReturn:
+        for key, value in data.items():
+            setattr(self, key, value)
+        db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_by_user_id(cls, user_id):
+        return cls.query.filter_by(user_id=user_id).all()
+
+    @classmethod
+    def find_by_hash(cls, hash):
+        return cls.query.filter_by(hash=hash).first()
+
+    def generate_hash(cls):
+        return uuid.uuid4().hex
+
+    def json(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "hash": self.hash
+        }
---
+++ backend/resources/application.py
@@ -0,0 +1,90 @@
+# Resource for building
+from email.mime import application
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_claims
+from models.application import ApplicationModel
+from models.user import UserModel
+from flask import request
+
+_application_parser_ = reqparse.RequestParser()
+_application_parser_.add_argument(
+    "user_id", type=int, required=True, help="user_id cannot be left blank!"
+)
+
+
+class Application(Resource):
+    @classmethod
+    def get(cls, id):
+        print("test")
+        application = ApplicationModel.find_by_id(id)
+        if application:
+            return application.json()
+        return {'message': 'Application not found'}, 404
+
+    @classmethod
+    @jwt_required
+    def put(cls, id):
+        data = request.get_json()
+        user_id = get_jwt_claims()["identity"]
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"message": "User not found!"}, 404
+        if not data:
+            return {'message': 'No input data provided'}, 400
+
+        application = ApplicationModel.find_by_id(id)
+        data["name"] = data.get("name", application.name)
+        application.update(data)
+        return application.json(), 201
+
+    @classmethod
+    @jwt_required
+    def delete(cls, id):
+        user_id = get_jwt_claims()["identity"]
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"message": "User not found!"}, 404
+
+        application = ApplicationModel.find_by_id(id)
+        if application:
+            application.delete_from_db()
+        return {}, 200
+
+class ApplicationList(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls):
+        id = get_jwt_claims()["identity"]
+        user = UserModel.find_by_id(id)
+        if not user:
+            return {"message": "User not found!"}, 404
+        
+        applications = ApplicationModel.find_by_user_id(id)
+        if application:
+            return {
+                'applications': [a.json() for a in applications]}
+        return {'message': 'Application not found'}, 404
+
+    @classmethod
+    @jwt_required
+    def post(cls):
+        data = request.get_json()
+        id = get_jwt_claims()["identity"]
+        user = UserModel.find_by_id(id)
+        if not user:
+            return {"message": "User not found!"}, 404
+        if not data:
+            return {'message': 'No input data provided'}, 400
+
+        application = ApplicationModel(name=data["name"], user_id=id)
+        application.save_to_db()
+        return application.json(), 201
+
+
+class ApplicationFind(Resource):
+    @classmethod
+    def get(cls, hash):
+        application = ApplicationModel.find_by_hash(hash)
+        if application:
+            return {'message': 'Application found!'}, 200
+        return {'message': 'Application not found'}, 404
