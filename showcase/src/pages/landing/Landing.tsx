2d2d6f6a5d4d36771f2c99d7b7b7d7c1452d6129
baris5d <baris@barisdede.com>
2022-08-29 15:03:59 +0300

    feat(showcase): implement Feedbacky in landing page

--- showcase/src/pages/landing/Landing.tsx
+++ showcase/src/pages/landing/Landing.tsx
@@ -1,20 +1,28 @@
-import Button from '../../common/components/button/Button';
-import Slogan from './components/slogan/Slogan';
-import Showcase from './components/showcase/Showcase';
-import './Landing.css';
+import * as React from 'react'
+import Button from '../../common/components/button/Button'
+import Slogan from './components/slogan/Slogan'
+import Showcase from './components/showcase/Showcase'
+import './Landing.css'
+import { initFeedbacky } from 'feedbacky'
 
 function Landing() {
-  return (
-    <div className="page landing">
-      <div className="container">
-        <Slogan />
-        <div style={{textAlign: 'center'}}>
-          <Button text="Get started" url="/getting-started" />
+    React.useEffect(() => {
+        initFeedbacky({
+            token: '123',
+        })
+    }, [])
+    return (
+        <div className="page landing">
+            <div className="container">
+                <Slogan />
+                <div style={{ textAlign: 'center' }}>
+                    <Button text="Get started" url="/getting-started" />
+                </div>
+                <Showcase />
+            </div>
+            <div id="feedbacky"></div>
         </div>
-        <Showcase />
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Landing;
+export default Landing
